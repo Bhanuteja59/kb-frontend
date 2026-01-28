d05b81fbace368abadbc0f6998ccb8bfa9e71013
Bunny <bhanutejareddy59@gmail.com>
2026-01-28 12:02:57 +0530

    Fix build errors: Resolve conflicts in next.config.js and regen package-lock.json

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     output: 'standalone',
     images: {
         remotePatterns: [
@@ -13,12 +13,11 @@ module.exports = {
             },
         ],
     },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["localhost:3000", "hoa-frontend-beryl.vercel.app"]
+        }
+    }
 };
-=======
-// frontend/next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: { allowedOrigins: ["localhost:3000", "hoa-frontend-beryl.vercel.app"] } }
-};
+
 module.exports = nextConfig;
->>>>>>> 542d1c491fbc7678e2c6a8cb451bac3dfa5cbe5d
